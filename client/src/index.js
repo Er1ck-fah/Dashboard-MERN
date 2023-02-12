b8f469be023d4a93f2d162417ed6e58216aab0a2
Erick KONGNE FAH <erickkongne@gmail.com>
2023-02-12 12:06:21 +0100

    modified:   client/src/App.js 	new file:   client/src/components/FlexBetween.jsx 	modified:   client/src/index.js 	modified:   client/src/scenes/navbar/index.jsx 	modified:   client/src/state/index.js 	modified:   client/src/theme.js 	modified:   server/data/index.js 	new file:   server/public/assets/info1.jpeg 	new file:   server/public/assets/info2.jpeg 	new file:   server/public/assets/info3.jpeg 	new file:   server/public/assets/info4.jpeg 	new file:   server/public/assets/linkedin.png 	new file:   server/public/assets/p1.jpeg 	new file:   server/public/assets/p10.jpeg 	new file:   server/public/assets/p11.jpeg 	new file:   server/public/assets/p12.jpeg 	new file:   server/public/assets/p13.jpeg 	new file:   server/public/assets/p2.jpeg 	new file:   server/public/assets/p3.jpeg 	new file:   server/public/assets/p4.jpeg 	new file:   server/public/assets/p5.jpeg 	new file:   server/public/assets/p6.jpeg 	new file:   server/public/assets/p7.jpeg 	new file:   server/public/assets/p8.jpeg 	new file:   server/public/assets/p9.jpeg 	new file:   server/public/assets/post1.jpeg 	new file:   server/public/assets/post2.jpeg 	new file:   server/public/assets/post3.jpeg 	new file:   server/public/assets/post4.jpeg 	new file:   server/public/assets/post5.jpeg 	new file:   server/public/assets/post6.jpeg 	new file:   server/public/assets/post7.jpeg 	new file:   server/public/assets/post8.jpeg 	new file:   server/public/assets/twitter.png

--- client/src/index.js
+++ client/src/index.js
@@ -17,6 +17,7 @@ import {
 } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { PersistGate } from 'redux-persist/integration/react';
+  
 
 const persistConfig = { key: "root", storage, version: 1 };
 const persistedReduced = persistReducer(persistConfig, authReducer);
--- server/data/index.js
+++ server/data/index.js
@@ -1,254 +1,285 @@
 import mongoose from "mongoose";
 
-const userIds=[
-    new mongoose.Types.ObjectId(),
-    new mongoose.Types.ObjectId(),
-    new mongoose.Types.ObjectId(),
-    new mongoose.Types.ObjectId(),
-    new mongoose.Types.ObjectId(),
-    new mongoose.Types.ObjectId(),
-    new mongoose.Types.ObjectId(),
-    new mongoose.Types.ObjectId(),
+const userIds = [
+  new mongoose.Types.ObjectId(),
+  new mongoose.Types.ObjectId(),
+  new mongoose.Types.ObjectId(),
+  new mongoose.Types.ObjectId(),
+  new mongoose.Types.ObjectId(),
+  new mongoose.Types.ObjectId(),
+  new mongoose.Types.ObjectId(),
+  new mongoose.Types.ObjectId(),
 ];
 
-export const users =[{
-    "_id": {
-      "$oid": "63e2d167a194765e4b29b479"
-    },
-    "firstName": "Leroy",
-    "email": "erickleroy@admin.com",
-    "password": "$2b$10$XPD3YcC.C2bQXWHlzd81de5kD4rtWO2NKja1ePhuGPkPXqoChls46",
-    "picturePath": "photo1.jpeg",
-    "friends": [],
-    "location": "Düsserdorf",
-    "occupation": "developper",
-    "viewedProfile": 9269,
-    "impressions": 8117,
-    "createdAt": {
-      "$date": {
-        "$numberLong": "1675809127760"
-      }
-    },
-    "updatedAt": {
-      "$date": {
-        "$numberLong": "1675809127760"
-      }
-    },
-    "__v": 0
-  },{
-    "_id": {
-      "$oid": "63e2ef7de56923a68ba005d4"
-    },
-    "firstName": "kongne",
-    "email": "erickleroy1@admin.com",
-    "password": "$2b$10$hAZWXNmZUKvWGm77ZY.MjuvwDwOArRQ7c/iFtGIK2dCGXoU3xxVgW",
-    "picturePath": "photo2.jpeg",
-    "friends": [],
-    "location": "Düsserdorf",
-    "occupation": "developper",
-    "viewedProfile": 2920,
-    "impressions": 5194,
-    "createdAt": {
-      "$date": {
-        "$numberLong": "1675816829099"
-      }
-    },
-    "updatedAt": {
-      "$date": {
-        "$numberLong": "1675816829099"
-      }
-    },
-    "__v": 0
-  },{
-    "_id": {
-      "$oid": "63e2ef8ee56923a68ba005d6"
-    },
-    "firstName": "fah",
-    "email": "erickleroy2@admin.com",
-    "password": "$2b$10$rEBMXQAIghRsZR.FAeaP5ukbjHIzqRDy6jlFAya.pWPT6VAF0uz.i",
-    "picturePath": "photo3.jpeg",
-    "friends": [],
-    "location": "Düsserdorf",
-    "occupation": "developper",
-    "viewedProfile": 5637,
-    "impressions": 6673,
-    "createdAt": {
-      "$date": {
-        "$numberLong": "1675816846179"
-      }
-    },
-    "updatedAt": {
-      "$date": {
-        "$numberLong": "1675816846179"
-      }
-    },
-    "__v": 0
-  },{
-    "_id": {
-      "$oid": "63e2efaee56923a68ba005d8"
-    },
-    "firstName": "ananfack",
-    "email": "ephraimananfack@admin.com",
-    "password": "$2b$10$jCVzpiuiRgYLHfAoo6zphO50p816YF1BKie2Spm3InTwDloBplIoa",
-    "picturePath": "photo4.jpeg",
-    "friends": [],
-    "location": "Düsserdorf",
-    "occupation": "developper",
-    "viewedProfile": 7958,
-    "impressions": 2258,
-    "createdAt": {
-      "$date": {
-        "$numberLong": "1675816878812"
-      }
-    },
-    "updatedAt": {
-      "$date": {
-        "$numberLong": "1675816878812"
-      }
-    },
-    "__v": 0
-  },{
-    "_id": {
-      "$oid": "63e2efbde56923a68ba005da"
-    },
-    "firstName": "kongne",
-    "email": "ephraimkongne@admin.com",
-    "password": "$2b$10$tk391FjyiIPimhiDmaVRGeL1MjwIp2gOkt53Oa7gKr0cERFTxdABS",
-    "picturePath": "photo5.jpeg",
-    "friends": [],
-    "location": "Düsserdorf",
-    "occupation": "developper",
-    "viewedProfile": 9020,
-    "impressions": 3140,
-    "createdAt": {
-      "$date": {
-        "$numberLong": "1675816893443"
-      }
-    },
-    "updatedAt": {
-      "$date": {
-        "$numberLong": "1675816893443"
-      }
-    },
-    "__v": 0
-  },{
-    "_id": {
-      "$oid": "63e2f009e56923a68ba005dc"
-    },
-    "firstName": "likondem",
-    "email": "julielikondem@admin.com",
-    "password": "$2b$10$bcnecBjw2qwNLIU2B.Neme61ZpmZshuEoSJrg0FgtCqxPZHydIZdq",
-    "picturePath": "photo6.jpeg",
-    "friends": [],
-    "location": "Düsserdorf",
-    "occupation": "developper",
-    "viewedProfile": 5075,
-    "impressions": 3470,
-    "createdAt": {
-      "$date": {
-        "$numberLong": "1675816969907"
-      }
-    },
-    "updatedAt": {
-      "$date": {
-        "$numberLong": "1675816969907"
-      }
-    },
-    "__v": 0
-  },{
-    "_id": {
-      "$oid": "63e2f022e56923a68ba005de"
-    },
-    "firstName": "lekeufack",
-    "email": "laurissalekeufack@admin.com",
-    "password": "$2b$10$dMDY0Vnyj/MTVVqhkHd1N.jvvC6jfWpjUVnMClYAi0hZWABBhf/Hm",
-    "picturePath": "photo7.jpeg",
-    "friends": [],
-    "location": "Düsserdorf",
-    "occupation": "developper",
-    "viewedProfile": 642,
-    "impressions": 8156,
-    "createdAt": {
-      "$date": {
-        "$numberLong": "1675816994765"
-      }
-    },
-    "updatedAt": {
-      "$date": {
-        "$numberLong": "1675816994765"
-      }
-    },
-    "__v": 0
-  },{
-    "_id": {
-      "$oid": "63e2f084e56923a68ba005e0"
-    },
-    "firstName": "ngoko",
-    "email": "germainengoko@admin.com",
-    "password": "$2b$10$SXnxcnAOjrKKrt.DSZ0DVO3wSbVoJ2SfCgKLJURVCMfiNAyT.Uhzy",
-    "picturePath": "photo8.jpeg",
-    "friends": [],
-    "location": "Düsserdorf",
-    "occupation": "developper",
-    "viewedProfile": 5177,
-    "impressions": 7536,
-    "createdAt": {
-      "$date": {
-        "$numberLong": "1675817092019"
-      }
-    },
-    "updatedAt": {
-      "$date": {
-        "$numberLong": "1675817092019"
-      }
-    },
-    "__v": 0
-  },{
-    "_id": {
-      "$oid": "63e2f092e56923a68ba005e2"
-    },
-    "firstName": "mvondo",
-    "email": "davidmvondo@admin.com",
-    "password": "$2b$10$DYl15oJM.Uj9RLYVS.R/WuV19/bwiCz/YCnSXBlkLDK0D/vbwD.BO",
-    "picturePath": "photo9.jpeg",
-    "friends": [],
-    "location": "Düsserdorf",
-    "occupation": "developper",
-    "viewedProfile": 6941,
-    "impressions": 4409,
-    "createdAt": {
-      "$date": {
-        "$numberLong": "1675817106501"
-      }
-    },
-    "updatedAt": {
-      "$date": {
-        "$numberLong": "1675817106501"
-      }
-    },
-    "__v": 0
-  },{
-    "_id": {
-      "$oid": "63e2f0a4e56923a68ba005e4"
-    },
-    "firstName": "chedjou",
-    "email": "rocelinchedjou@admin.com",
-    "password": "$2b$10$ysWG19ehWMc/X4DQu/I88euq4Ck.6YYJamw4Cf7V1sqRDLOLuRBl.",
-    "picturePath": "photo10.jpeg",
-    "friends": [],
-    "location": "Düsserdorf",
-    "occupation": "developper",
-    "viewedProfile": 2052,
-    "impressions": 3252,
-    "createdAt": {
-      "$date": {
-        "$numberLong": "1675817124392"
-      }
-    },
-    "updatedAt": {
-      "$date": {
-        "$numberLong": "1675817124392"
-      }
-    },
-    "__v": 0
-  }];
+export const users = [
+  {
+    _id: userIds[0],
+    firstName: "test",
+    lastName: "me",
+    email: "aaaaaaa@gmail.com",
+    password: "$2b$10$dsasdgsagasda//G9JxQ4bQ8KXf4OAIe/X/AK9skyWUy",
+    picturePath: "p11.jpeg",
+    friends: [],
+    location: "San Fran, CA",
+    occupation: "Software Engineer",
+    viewedProfile: 14561,
+    impressions: 888822,
+    createdAt: 1115211422,
+    updatedAt: 1115211422,
+    __v: 0,
+  },
+  {
+    _id: userIds[1],
+    firstName: "Steve",
+    lastName: "Ralph",
+    email: "thataaa@gmail.com",
+    password: "$!FEAS@!O)_IDJda//G9JxQ4bQ8KXf4OAIe/X/AK9skyWUy",
+    picturePath: "p3.jpeg",
+    friends: [],
+    location: "New York, CA",
+    occupation: "Degenerate",
+    viewedProfile: 12351,
+    impressions: 55555,
+    createdAt: 1595589072,
+    updatedAt: 1595589072,
+    __v: 0,
+  },
+  {
+    _id: userIds[2],
+    firstName: "Some",
+    lastName: "Guy",
+    email: "someguy@gmail.com",
+    password: "da39a3ee5e6b4b0d3255bfef95601890afd80709",
+    picturePath: "p4.jpeg",
+    friends: [],
+    location: "Canada, CA",
+    occupation: "Data Scientist Hacker",
+    viewedProfile: 45468,
+    impressions: 19986,
+    createdAt: 1288090662,
+    updatedAt: 1288090662,
+    __v: 0,
+  },
+  {
+    _id: userIds[3],
+    firstName: "Whatcha",
+    lastName: "Doing",
+    email: "whatchadoing@gmail.com",
+    password: "$2b$10$dsasdgsagasda//G9JxQ4bQ8KXf4OAIe/X/AK9skyWUy",
+    picturePath: "p6.jpeg",
+    friends: [],
+    location: "Korea, CA",
+    occupation: "Educator",
+    viewedProfile: 41024,
+    impressions: 55316,
+    createdAt: 1219214568,
+    updatedAt: 1219214568,
+    __v: 0,
+  },
+  {
+    _id: userIds[4],
+    firstName: "Jane",
+    lastName: "Doe",
+    email: "janedoe@gmail.com",
+    password: "$2b$10$dsasdgsagasda//G9JxQ4bQ8KXf4OAIe/X/AK9skyWUy",
+    picturePath: "p5.jpeg",
+    friends: [],
+    location: "Utah, CA",
+    occupation: "Hacker",
+    viewedProfile: 40212,
+    impressions: 7758,
+    createdAt: 1493463661,
+    updatedAt: 1493463661,
+    __v: 0,
+  },
+  {
+    _id: userIds[5],
+    firstName: "Harvey",
+    lastName: "Dunn",
+    email: "harveydunn@gmail.com",
+    password: "$2b$10$dsasdgsagasda//G9JxQ4bQ8KXf4OAIe/X/AK9skyWUy",
+    picturePath: "p7.jpeg",
+    friends: [],
+    location: "Los Angeles, CA",
+    occupation: "Journalist",
+    viewedProfile: 976,
+    impressions: 4658,
+    createdAt: 1381326073,
+    updatedAt: 1381326073,
+    __v: 0,
+  },
+  {
+    _id: userIds[6],
+    firstName: "Carly",
+    lastName: "Vowel",
+    email: "carlyvowel@gmail.com",
+    password: "$2b$10$dsasdgsagasda//G9JxQ4bQ8KXf4OAIe/X/AK9skyWUy",
+    picturePath: "p8.jpeg",
+    friends: [],
+    location: "Chicago, IL",
+    occupation: "Nurse",
+    viewedProfile: 1510,
+    impressions: 77579,
+    createdAt: 1714704324,
+    updatedAt: 1642716557,
+    __v: 0,
+  },
+  {
+    _id: userIds[7],
+    firstName: "Jessica",
+    lastName: "Dunn",
+    email: "jessicadunn@gmail.com",
+    password: "$2b$10$dsasdgsagasda//G9JxQ4bQ8KXf4OAIe/X/AK9skyWUy",
+    picturePath: "p9.jpeg",
+    friends: [],
+    location: "Washington, DC",
+    occupation: "A Student",
+    viewedProfile: 19420,
+    impressions: 82970,
+    createdAt: 1369908044,
+    updatedAt: 1359322268,
+    __v: 0,
+  },
+];
+
+export const posts = [
+  {
+    _id: new mongoose.Types.ObjectId(),
+    userId: userIds[1],
+    firstName: "Steve",
+    lastName: "Ralph",
+    location: "New York, CA",
+    description: "Some really long random description",
+    picturePath: "post1.jpeg",
+    userPicturePath: "p3.jpeg",
+    likes: new Map([
+      [userIds[0], true],
+      [userIds[2], true],
+      [userIds[3], true],
+      [userIds[4], true],
+    ]),
+    comments: [
+      "random comment",
+      "another random comment",
+      "yet another random comment",
+    ],
+  },
+  {
+    _id: new mongoose.Types.ObjectId(),
+    userId: userIds[3],
+    firstName: "Whatcha",
+    lastName: "Doing",
+    location: "Korea, CA",
+    description:
+      "Another really long random description. This one is longer than the previous one.",
+    picturePath: "post2.jpeg",
+    userPicturePath: "p6.jpeg",
+    likes: new Map([
+      [userIds[7], true],
+      [userIds[4], true],
+      [userIds[1], true],
+      [userIds[2], true],
+    ]),
+    comments: [
+      "one more random comment",
+      "and another random comment",
+      "no more random comments",
+      "I lied, one more random comment",
+    ],
+  },
+  {
+    _id: new mongoose.Types.ObjectId(),
+    userId: userIds[4],
+    firstName: "Jane",
+    lastName: "Doe",
+    location: "Utah, CA",
+    description:
+      "This is the last really long random description. This one is longer than the previous one.",
+    picturePath: "post3.jpeg",
+    userPicturePath: "p5.jpeg",
+    likes: new Map([
+      [userIds[1], true],
+      [userIds[6], true],
+      [userIds[3], true],
+      [userIds[5], true],
+    ]),
+    comments: [
+      "one more random comment",
+      "I lied, one more random comment",
+      "I lied again, one more random comment",
+      "Why am I doing this?",
+      "I'm bored",
+    ],
+  },
+  {
+    _id: new mongoose.Types.ObjectId(),
+    userId: userIds[5],
+    firstName: "Harvey",
+    lastName: "Dunn",
+    location: "Los Angeles, CA",
+    description:
+      "This is the last really long random description. This one is longer than the previous one. Man I'm bored. I'm going to keep typing until I run out of things to say.",
+    picturePath: "post4.jpeg",
+    userPicturePath: "p7.jpeg",
+    likes: new Map([
+      [userIds[1], true],
+      [userIds[6], true],
+      [userIds[3], true],
+    ]),
+    comments: [
+      "I lied again, one more random comment",
+      "Why am I doing this?",
+      "I'm bored",
+      "I'm still bored",
+      "All I want to do is play video games",
+      "I'm going to play video games",
+    ],
+  },
+  {
+    _id: new mongoose.Types.ObjectId(),
+    userId: userIds[6],
+    firstName: "Carly",
+    lastName: "Vowel",
+    location: "Chicago, IL",
+    description:
+      "Just a short description. I'm tired of typing. I'm going to play video games now.",
+    picturePath: "post5.jpeg",
+    userPicturePath: "p8.jpeg",
+    likes: new Map([
+      [userIds[1], true],
+      [userIds[3], true],
+      [userIds[5], true],
+      [userIds[7], true],
+    ]),
+    comments: [
+      "I lied again, one more random comment",
+      "Why am I doing this?",
+      "Man I'm bored",
+      "What should I do?",
+      "I'm going to play video games",
+    ],
+  },
+  {
+    _id: new mongoose.Types.ObjectId(),
+    userId: userIds[7],
+    firstName: "Jessica",
+    lastName: "Dunn",
+    location: "Washington, DC",
+    description:
+      "For the last time, I'm going to play video games now. I'm tired of typing. I'm going to play video games now.",
+    picturePath: "post6.jpeg",
+    userPicturePath: "p9.jpeg",
+    likes: new Map([
+      [userIds[1], true],
+      [userIds[2], true],
+    ]),
+
+    comments: [
+      "Can I play video games now?",
+      "No let's actually study",
+      "Never mind, I'm going to play video games",
+      "Stop it.",
+      "Michael, stop it.",
+    ],
+  },
+];
